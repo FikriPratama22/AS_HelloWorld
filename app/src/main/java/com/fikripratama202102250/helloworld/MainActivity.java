0d0fdb171e5bf4feda93321ce7499784f637fe05
Fikri R. Pratama <93368110+FikriPratama22@users.noreply.github.com>
2023-03-08 10:33:16 +0700

    Initial commit

---
+++ app/src/main/java/com/fikripratama202102250/helloworld/MainActivity.java
@@ -0,0 +1,14 @@
+package com.fikripratama202102250.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
